3d1feeb340177a03591d5f24913001a41be45923
Jordan Matelsky <j6k4m8@gmail.com>
2021-01-27 09:21:10 -0500

    Throw errors for the user when an API issue occurs

--- motif-studio/src/App.tsx
+++ motif-studio/src/App.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import { MotifStudio } from "./MotifStudio";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-    return <MotifStudio />;
+    return (
+        <>
+            <MotifStudio />
+            <ToastContainer />
+        </>
+    );
 }
 
 export default App;
