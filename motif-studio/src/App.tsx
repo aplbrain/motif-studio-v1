51829f89dec689340659b02c02ce2c70286cc776
Jordan Matelsky <j6k4m8@gmail.com>
2021-05-27 10:48:39 -0400

    Add saving menu

--- motif-studio/src/App.tsx
+++ motif-studio/src/App.tsx
@@ -1,17 +1,104 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 
 import { MotifStudio } from "./MotifStudio";
-import { ToastContainer } from "react-toastify";
-import { Dropdown, Nav, Navbar } from "react-bootstrap";
+import { toast, ToastContainer } from "react-toastify";
+import {
+    Button,
+    Dropdown,
+    FormControl,
+    InputGroup,
+    ListGroup,
+    Modal,
+    Nav,
+    Navbar,
+} from "react-bootstrap";
+import { LocalStorageMotifStore } from "./store";
 
 function App() {
     let [view, setView] = useState("Build");
+    let studio = useRef(null);
+    let [saveModalVisible, setSaveModalVisible] = useState(false);
+    let [savingMotifName, setSavingMotifName] = useState("");
+
+    let db = new LocalStorageMotifStore();
+
+    // @ts-ignore
+    let motifText = studio?.current?.state?.motifText;
+
+    let savedMotifs = Object.values(db.list()).sort((left, right) =>
+        new Date(left.savedDate)
+            .toISOString()
+            .localeCompare(new Date(right.savedDate).toISOString())
+    );
+
+    let handleSave = () => {
+        db.save(savingMotifName, {
+            name: savingMotifName,
+            motifText,
+            savedDate: new Date(),
+        });
+        setSaveModalVisible(false);
+        toast.success(`Saved '${savingMotifName}'.`);
+    };
+
+    let savedMotifsListGroup = (
+        <ListGroup>
+            {savedMotifs.map((motif) => (
+                <ListGroup.Item
+                    key={motif.name}
+                    onClick={(ev) => setSavingMotifName(motif.name)}
+                >
+                    {motif.name}{" "}
+                    <div
+                        className="float-right"
+                        style={{ display: "inline-block" }}
+                    >
+                        {motif.savedDate.toLocaleString()}
+                    </div>
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+    );
+
+    let saveModal = (
+        <>
+            <Modal
+                show={saveModalVisible}
+                onHide={() => setSaveModalVisible(false)}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Save this motif</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>{savedMotifsListGroup}</Modal.Body>
+
+                <Modal.Footer>
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            placeholder="Filename"
+                            aria-label="Filename"
+                            onChange={(ev) =>
+                                setSavingMotifName(ev.target.value)
+                            }
+                            value={savingMotifName}
+                        />
+                        <InputGroup.Append>
+                            <Button onClick={handleSave} variant="primary">
+                                Save
+                            </Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
     return (
         <>
+            {saveModal}
             <Navbar bg="primary" variant="dark">
                 <Nav className="mr-auto ">
                     <div className="d-flex">
@@ -26,7 +113,11 @@ function App() {
 
                             <Dropdown.Menu>
                                 <Dropdown.Item>Open</Dropdown.Item>
-                                <Dropdown.Item>Save</Dropdown.Item>
+                                <Dropdown.Item
+                                    onClick={() => setSaveModalVisible(true)}
+                                >
+                                    Save
+                                </Dropdown.Item>
                                 <Dropdown.Divider />
                                 <Dropdown.Item>Share</Dropdown.Item>
                             </Dropdown.Menu>
@@ -41,7 +132,7 @@ function App() {
                 </Nav>
                 <Navbar.Brand href="#home">Motif Studio</Navbar.Brand>
             </Navbar>
-            <MotifStudio requestedView={view} />
+            <MotifStudio requestedView={view} ref={studio} />
             <ToastContainer />
         </>
     );
---
+++ motif-studio/src/store.tsx
@@ -0,0 +1,46 @@
+type MotifPayload = {
+    motifText: string;
+    savedDate: Date;
+    name: string;
+};
+
+interface MotifStore {
+    save(name: string, motif: MotifPayload): void;
+    load(name: string): MotifPayload;
+}
+
+class LocalStorageMotifStore implements MotifStore {
+    list(): MotifPayload[] {
+        return Object.values(this._getFromLocalStorage());
+    }
+    dictionaryKey: string;
+
+    constructor(dictionaryKey?: string) {
+        this.dictionaryKey = dictionaryKey || "savedMotifs";
+    }
+
+    _getFromLocalStorage() {
+        return (
+            JSON.parse(localStorage.getItem(this.dictionaryKey) || "{}") || {}
+        );
+    }
+    _saveToLocalStorage(payload: { [name: string]: MotifPayload }) {
+        return localStorage.setItem(
+            this.dictionaryKey,
+            JSON.stringify(payload)
+        );
+    }
+
+    save(name: string, motif: MotifPayload): void {
+        let currentStore = this._getFromLocalStorage();
+        currentStore[name] = motif;
+        this._saveToLocalStorage(currentStore);
+    }
+
+    load(name: string): MotifPayload {
+        let currentStore = this._getFromLocalStorage();
+        return currentStore[name];
+    }
+}
+
+export { LocalStorageMotifStore };
