eb9d79416771713fe8c4d9155b6c631b6186b158
Jordan Matelsky <j6k4m8@gmail.com>
2021-05-26 22:51:00 -0400

    Add menubar

--- motif-studio/src/App.tsx
+++ motif-studio/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,11 +6,42 @@ import "react-toastify/dist/ReactToastify.css";
 
 import { MotifStudio } from "./MotifStudio";
 import { ToastContainer } from "react-toastify";
+import { Dropdown, Nav, Navbar } from "react-bootstrap";
 
 function App() {
+    let [view, setView] = useState("Build");
     return (
         <>
-            <MotifStudio />
+            <Navbar bg="primary" variant="dark">
+                <Nav className="mr-auto ">
+                    <div className="d-flex">
+                        <Dropdown>
+                            <Dropdown.Toggle
+                                as={Nav.Link}
+                                active
+                                id="dropdown-basic"
+                            >
+                                File
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item>Open</Dropdown.Item>
+                                <Dropdown.Item>Save</Dropdown.Item>
+                                <Dropdown.Divider />
+                                <Dropdown.Item>Share</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Nav.Link active onClick={() => setView("Build")}>
+                            Build
+                        </Nav.Link>
+                        <Nav.Link active onClick={() => setView("Run")}>
+                            Run
+                        </Nav.Link>
+                    </div>
+                </Nav>
+                <Navbar.Brand href="#home">Motif Studio</Navbar.Brand>
+            </Navbar>
+            <MotifStudio requestedView={view} />
             <ToastContainer />
         </>
     );
--- motif-studio/src/Config.tsx
+++ motif-studio/src/Config.tsx
@@ -1,6 +1,7 @@
 export const Config = {
     api: {
         throttleMs: 1000,
-        baseURL: "http://localhost:5000",
+        baseURL: "http://motifstudio:5000",
+        // baseURL: "http://localhost:5000",
     },
 };
